cdb2d7c81be3df5effb5dbb99fc9d17f764c228c
Piotr <piotrwc49@gmail.com>
2024-06-14 01:03:20 +0200

    callback

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -33,7 +33,7 @@ export default function Login() {
     await supabaseClient.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `https://urlsypnpyiqmnsmmysrk.supabase.co/auth/v1/callback`,
+        redirectTo: `https://payme-wfdv.onrender.com/callback`,
       },
     })
   }
