a84fa95ee31f970da05d230b843e020560c258d3
Piotr <piotrwc49@gmail.com>
2024-07-11 14:50:16 +0200

    delete useDarkMode

--- src/hooks/useDarkMode.tsx
+++
@@ -1,6 +0,0 @@
-import {useContext} from "react";
-import {DarkModeContext} from "@/context/ThemeProvider.tsx";
-
-const useDarkMode = () => useContext(DarkModeContext)
-
-export default useDarkMode
